18f0e59dc780e403c54873a8814625851f03ed90
BJ Chatterson <wjchat@umich.edu>
2018-01-14 20:52:54 -0500

    finished hw 1

--- SI364W18_HW1.py
+++ SI364W18_HW1.py
@@ -6,12 +6,16 @@
 
 ## List below here, in a comment/comments, the people you worked with on this assignment AND any resources you used to find code (50 point deduction for not doing so). If none, write "None".
 
+##phill brown
+##used stackoverflow
 
+import requests
+import json
 
 ## [PROBLEM 1] - 150 points
 ## Below is code for one of the simplest possible Flask applications. Edit the code so that once you run this application locally and go to the URL 'http://localhost:5000/class', you see a page that says "Welcome to SI 364!"
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 app.debug = True
 
@@ -19,9 +23,118 @@ app.debug = True
 def hello_to_you():
     return 'Hello!'
 
+@app.route('/class')
+def welcome():
+	return 'Welcome to SI 364!'
+
+@app.route('/movie/<movie_name>')  ##gets movie information from itunes api and presents it in very ugly text format
+def movie_find(movie_name):
+	baseurl ='https://itunes.apple.com/search?'
+	param_dict = {'term': movie_name, 'entity': 'movie'}
+	movie_info = requests.get(baseurl, params = param_dict)
+
+	return(movie_info.text)
+
+@app.route('/question')
+def number_question():
+	nub_ques = """<!DOCTYPE html>
+	<html>
+	<body>
+	<form action = "/result" method ="POST">
+		<p>Enter a number pls</p>
+		<input type = "text" name ="num" value="Enter your favorite number">
+		<br>
+		<input type = "submit" value = "Click to Submit">
+	</form>
+	</break>
+	</html>"""
+
+	return nub_ques
+
+
+@app.route('/result', methods =['POST', 'GET'])
+def num_result():
+	if request.method =='POST':
+		number = request.form['num']
+	s = 'Double your favorite number is {}'.format(int(number)*2)
+
+	return s
+
+@app.route('/problem4form', methods = ['POST', 'GET'])
+def problem4():
+	d = """<!DOCTYPE html>
+	<html>
+	<body>
+	<form action = '/problem4form' method = 'POST'>
+		<input type = 'text' name = 'artist' value = 'musician'>
+		<br>
+		<p>What would you like?</p>
+		<p>5 songs</p>
+			<input type = 'checkbox' name = 'songs' value = 'songs'>
+		<p>5 albums</p>
+			<input type = 'checkbox' name = 'albums' value = 'albums'>
+		<br>
+		<input type = 'submit' value = 'click to submit'>
+		
+	</form
+	</body>
+	</html>"""
+
+	final_dic= {}
+	if request.method == 'POST':
+		artist_name = request.form['artist']
+
+		baseurl = 'https://itunes.apple.com/search?'
+		param_dict = {'term': artist_name, 'entity': 'musicArtist'}
+		artist_info = requests.get(baseurl, params = param_dict)
+
+		artist_info = artist_info.json()
+		
+		names = []
+
+		for each in artist_info['results']:
+			names.append(each['artistName'])
+		
+		if request.form.get('songs'):
+			param_dict = {'term': artist_name, 'entity': 'song'}
+			song_list = requests.get(baseurl, params = param_dict)
+			song_list = song_list.json()
+
+			thesongs = []
+			for each in song_list['results'][0:5]:
+				thesongs.append(each['trackCensoredName'])
+			final_dic['Songs'] = thesongs
+
+		if request.form.get('albums'):
+			param_dict = {'term': artist_name, 'entity': 'album'}
+			albums = requests.get(baseurl, params = param_dict)
+			albums = albums.json()
+
+			thealbums = []
+			for each in albums['results'][0:5]:
+				thealbums.append(each['collectionName'])
+			final_dic['Albums'] = thealbums 
+
+		return d + str(final_dic)
+
+
+	else:
+
+		return d
+
+
+
+	
+
+	
+
+
+
 
 if __name__ == '__main__':
     app.run()
+
+
 
 
 ## [PROBLEM 2] - 250 points
